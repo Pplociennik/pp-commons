14aee98eb589a7834f894cf89ea5987df0dbb44f
pplociennik <pplociennik097@gmail.com>
2026-01-26 20:57:27 +0100

    [NO_JIRA] Added ClientActionFlag to the ErrorResponseDto; added a builder to the ErrorResponseDto

--- src/main/java/com/github/pplociennik/commons/dto/ErrorResponseDto.java
+++ src/main/java/com/github/pplociennik/commons/dto/ErrorResponseDto.java
@@ -26,8 +26,8 @@
 
 package com.github.pplociennik.commons.dto;
 
+import com.github.pplociennik.commons.system.client.ClientActionFlag;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
@@ -45,7 +45,6 @@ import java.time.ZonedDateTime;
 )
 @EqualsAndHashCode( callSuper = true )
 @Data
-@AllArgsConstructor
 public class ErrorResponseDto extends BaseAbstractExtendableDto {
 
     @Schema(
@@ -71,5 +70,124 @@ public class ErrorResponseDto extends BaseAbstractExtendableDto {
             example = "2024-03-23T18:00:00.000"
     )
     private ZonedDateTime errorTime;
+
+    @Schema(
+            description = "A client action flag determining an action to be executed on the client's side after receiving the response.",
+            example = "VERIFY_USER_EMAIL"
+    )
+    private ClientActionFlag clientActionFlag;
+
+    /**
+     * Constructs a new instance of ErrorResponseDto with the specified parameters.
+     *
+     * @param apiPath
+     *         the API path where the error occurred
+     * @param errorCode
+     *         the HTTP status code representing the error
+     * @param errorMessage
+     *         the detailed error message describing the issue
+     * @param errorTime
+     *         the timestamp of when the error occurred
+     */
+    public ErrorResponseDto( String apiPath, HttpStatus errorCode, String errorMessage, ZonedDateTime errorTime ) {
+        this.apiPath = apiPath;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.errorTime = errorTime;
+    }
+
+    /**
+     * Constructs a new instance of ErrorResponseDto with the specified parameters.
+     *
+     * @param apiPath
+     *         the API path where the error occurred
+     * @param errorCode
+     *         the HTTP status code representing the error
+     * @param errorMessage
+     *         the detailed error message describing the issue
+     * @param errorTime
+     *         the timestamp of when the error occurred
+     * @param clientActionFlag
+     *         the client action flag associated with the error
+     */
+    public ErrorResponseDto( String apiPath, HttpStatus errorCode, String errorMessage, ZonedDateTime errorTime, ClientActionFlag clientActionFlag ) {
+        this.apiPath = apiPath;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.errorTime = errorTime;
+        this.clientActionFlag = clientActionFlag;
+    }
+
+    /**
+     * Creates a new Builder instance for constructing an ErrorResponseDto object.
+     *
+     * @param aApiPath
+     *         the API path that was invoked by the client
+     * @param aErrorCode
+     *         the error code representing the occurred error
+     * @param aErrorMessage
+     *         the error message describing the error
+     * @param aErrorTime
+     *         the timestamp when the error occurred
+     * @return a new Builder instance initialized with the provided values
+     */
+    public static Builder builder( String aApiPath, HttpStatus aErrorCode, String aErrorMessage, ZonedDateTime aErrorTime ) {
+        return new Builder( aApiPath, aErrorCode, aErrorMessage, aErrorTime );
+    }
+
+    /**
+     * Builder class responsible for constructing instances of {@link ErrorResponseDto}.
+     * This class provides a fluent API for setting specific parameters and creating
+     * an instance of the ErrorResponseDto.
+     */
+    public static class Builder {
+        private String apiPath;
+        private HttpStatus errorCode;
+        private String errorMessage;
+        private ZonedDateTime errorTime;
+        private ClientActionFlag clientActionFlag;
+
+        /**
+         * Constructs a new Builder instance with the specified parameters.
+         *
+         * @param apiPath
+         *         the API path associated with the error
+         * @param errorCode
+         *         the HTTP status code representing the error
+         * @param errorMessage
+         *         the error message providing details about the issue
+         * @param errorTime
+         *         the timestamp when the error occurred
+         */
+        Builder( String apiPath, HttpStatus errorCode, String errorMessage, ZonedDateTime errorTime ) {
+            this.apiPath = apiPath;
+            this.errorCode = errorCode;
+            this.errorMessage = errorMessage;
+            this.errorTime = errorTime;
+        }
+
+        /**
+         * Sets the client action flag for the builder.
+         *
+         * @param clientActionFlag
+         *         the client action flag to associate with the error response
+         * @return the builder instance for method chaining
+         */
+        public final Builder withClientActionFlag( ClientActionFlag clientActionFlag ) {
+            this.clientActionFlag = clientActionFlag;
+            return this;
+        }
+
+        /**
+         * Constructs an instance of {@link ErrorResponseDto} using the parameters
+         * that have been set in the {@code Builder}.
+         *
+         * @return a new instance of {@link ErrorResponseDto} containing the provided API path,
+         * error code, error message, error timestamp, and client action flag.
+         */
+        public final ErrorResponseDto build() {
+            return new ErrorResponseDto( this.apiPath, this.errorCode, this.errorMessage, this.errorTime, this.clientActionFlag );
+        }
+    }
 
 }
